b1bb9c676ab8e07b9f2eebb40aba3529ed75605e
csd5615 <csd5615@csd.uoc.gr>
2024-03-15 22:38:35 +0200

    class sphere creation

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,13 +1,50 @@
 /**
  * @file Assignment2.cpp
- *  
+ *
  * @brief Assignment 2 - HY-150
- * 
- * @author: csdXXXX
+ *
+ * @author: csd5615
  */
 
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
 #include <iostream>
 #include <fstream>
+
+class Sphere
+{
+    vec3 position;
+    vec3 color;
+    float radius;
+
+    Sphere(vec3 position, vec3 color, float radius)
+    {
+        this->position = position;
+        this->color = color;
+        this->radius = radius;
+    }
+
+    bool hit_sphere(const ray &ray_hit, double &distance)
+    {
+        vec3 oc = ray_hit.origin() - position;
+        auto a = dot(ray_hit.direction(), ray_hit.direction());
+        auto b = 2.0 * dot(oc, ray_hit.direction());
+        auto c = dot(oc, oc) - radius * radius;
+        auto discriminant = b * b - 4 * a * c;
+
+        if (discriminant >= 0)
+        {
+            double tmp_dist= oc.length()-radius;
+            if (tmp_dist>distance){
+                distance=tmp_dist;
+                return true;
+            }
+            
+        }
+        return false;
+    };
+};
 
 int main()
 {
