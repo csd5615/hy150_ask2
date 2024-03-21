be85ce7e9d2136fffdac40fcc9c315cc84e41b0e
csd5615 <csd5615@csd.uoc.gr>
2024-03-22 01:21:44 +0200

    final changes

--- Sphere.cpp
+++ Sphere.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 #include "Classes.h"
 using namespace std;
-class Bad_color{
-
+class Bad_color
+{
 };
 class Sphere
 {
@@ -15,7 +15,7 @@ class Sphere
     vec3 color;
     float radius;
 
-    public:
+public:
     vec3 bad_color;
 
 public:
@@ -25,72 +25,68 @@ public:
     vec3 c() const { return color; };
     void c(vec3 color)
     {
-        try
-        {
-            for (int i = 0; i < 3; i++)
+    
+            try
             {
-                if (color.e[i] >= 0 && color.e[i] < 255)
+                for (int i = 0; i < 3; i++)
                 {
-                    this->color = color;
+                    if (color.e[i] < 0 || color.e[i] > 1)
+                    {
+                        throw Bad_color(); // Throw exception if any element is out of range
+                    }
                 }
-                else
-                {
-                    throw(color);
-                }
-            }
-        }
 
-        catch (Bad_color &error)
-        {
-            cout << "Oops! Colors must be between 0-255.";
-        }
+                this->color = color;
+            }
+            catch (Bad_color &)
+            {
+                cout << "Oops! Your colors weren't between 0-1."<<'\n';
+            }
+        
     };
 
-    float r() { return radius; };
-    void r(float radius) { this->radius = radius; };
+        float r() { return radius; };
+        void r(float radius) { this->radius = radius; };
 
-    Sphere(vec3 position, vec3 color, float radius)
-    {
-        this->position = position;
-        try
+        Sphere(vec3 position, vec3 color, float radius)
         {
-            for (int i = 0; i < 3; i++)
+            this->position = position;
+            try
             {
-                if (color.e[i] >= 0 && color.e[i] < 255)
+                for (int i = 0; i < 3; i++)
                 {
-                    this->color = color;
+                    if (color.e[i] < 0 || color.e[i] > 1)
+                    {
+                        throw Bad_color(); // Throw exception if any element is out of range
+                    }
                 }
-                else
+
+                this->color = color;
+            }
+            catch (Bad_color &)
+            {
+                cout << "Oops! Colors must be between 0-1.";
+            }
+            this->radius = radius;
+        }
+
+        bool hit_sphere(const ray &ray_hit, double &distance)
+        {
+            vec3 oc = ray_hit.origin() - position;
+            auto a = dot(ray_hit.direction(), ray_hit.direction());
+            auto b = 2.0 * dot(oc, ray_hit.direction());
+            auto c = dot(oc, oc) - radius * radius;
+            auto discriminant = b * b - 4 * a * c;
+
+            if (discriminant >= 0)
+            {
+                double tmp_dist = (-b - sqrt(discriminant)) / 2 * a;
+                if (tmp_dist < distance || distance == 0)
                 {
-                    throw(color);
+                    distance = tmp_dist;
+                    return true;
                 }
             }
-        }
-
-        catch (Bad_color &error)
-        {
-            cout << "Oops! Colors must be between 0-255.";
-        }
-        this->radius = radius;
-    }
-
-    bool hit_sphere(const ray &ray_hit, double &distance)
-    {
-        vec3 oc = ray_hit.origin() - position;
-        auto a = dot(ray_hit.direction(), ray_hit.direction());
-        auto b = 2.0 * dot(oc, ray_hit.direction());
-        auto c = dot(oc, oc) - radius * radius;
-        auto discriminant = b * b - 4 * a * c;
-
-        if (discriminant >= 0)
-        {
-            double tmp_dist = (-b-sqrt(discriminant))/2*a;
-            if (tmp_dist < distance || distance == 0)
-            {
-                distance = tmp_dist;
-                return true;
-            }
-        }
-        return false;
+            return false;
+        };
     };
-};
--- World.cpp
+++ World.cpp
@@ -14,23 +14,38 @@ class World
     string file_name;
     int image_height;
     int image_width;
+
 public:
     World(vec3 SkyColor, string file_name)
     {
-        this->SkyColor = SkyColor;
+        try
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (SkyColor.e[i] < 0 || SkyColor[i] > 1)
+                {
+                    throw Bad_color(); // Throw exception if any element is out of range
+                }
+            }
+            this->SkyColor = SkyColor;
+        }
+        catch (Bad_color &)
+        {
+            cout << "Oops! Your colors weren't between 0-1."<<"\n";
+        }
         this->file_name = file_name;
     }
-vector<Sphere> S(){return Spheres;};
+    vector<Sphere> S() { return Spheres; };
     void add_sphere(Sphere &new_sphere)
     {
         Spheres.push_back(new_sphere);
     }
 
-    void Render(Camera &new_camera, fstream* fout)
+    void Render(Camera &new_camera, fstream *fout)
     {
         *fout << "P3\n"
-         << new_camera.i_w() << ' ' << new_camera.i_h() << "\n255\n";
-        
+              << new_camera.i_w() << ' ' << new_camera.i_h() << "\n255\n";
+
         for (int j = 0; j < new_camera.i_h(); j++) // y coordinates
         {
             for (int i = 0; i < new_camera.i_w(); i++) // x coordinates
@@ -42,18 +57,20 @@ vector<Sphere> S(){return Spheres;};
                 double distance = 0;
                 for (int a = 0; a < Spheres.size(); a++)
                 {
-                    
+
                     if (Spheres[a].hit_sphere(r, distance))
                     {
-                        //cout << "hello";
+
                         pixel_color = color(Spheres[a].c());
                     }
                 }
-                
-                //cout<<" "<<j;
-                 write_color(*fout, pixel_color);
+
+                write_color(*fout, pixel_color);
             }
+
+            cout << "\nGenerating image: " << ((float(j) + 1) / new_camera.i_h()) * 100 << "%";
+               
         };
-        cout<< "DONE!";
+        cout << "\nDONE!";
     }
 };
